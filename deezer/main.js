44bd46712f049ef2f963eacce18501512c832e8f
Ordissimo <thierry@ordissimo.com>
2018-05-03 12:58:02 +0200

    Update main.js
    Update path libpepflashplayer.so

--- deezer/main.js
+++ deezer/main.js
@@ -17,7 +17,7 @@ try {
     fs.mkdirSync(configPath, 755);
 }
 
-app.commandLine.appendSwitch('ppapi-flash-path', "/opt/google/chrome-unstable/PepperFlash/libpepflashplayer.so");
+app.commandLine.appendSwitch('ppapi-flash-path', "/usr/lib/adobe-flashplugin/libpepflashplayer.so");
 
 // app.commandLine.appendSwitch('ppapi-flash-path', path.resolve(__dirname, 'plugins/libpepflashplayer.so'));
 // app.commandLine.appendSwitch('ppapi-flash-version', JSON.parse(require("fs").readFileSync(path.resolve(__dirname, "plugins/manifest.json")).toString()).version);
